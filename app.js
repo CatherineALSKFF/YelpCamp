848bd0f2c7c101bc91fa8c1fb67e4538f3552aca
Catherine Al Skaff <catherine.alskaf@gmail.com>
2023-02-09 13:54:43 +0100

    Common Security Protocoles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config()
 }
+
 const express= require('express');
 const path= require('path');
 const mongoose= require('mongoose');
@@ -8,7 +9,6 @@ const ejsMate= require('ejs-mate');
 const session= require('express-session');
 const flash= require('connect-flash');
 const ExpressError= require('./utils/ExpressError');
-const Joi= require('joi')
 const methodOverride = require('method-override');
 const passport= require('passport');
 const LocalStrategy= require('passport-local');
@@ -16,7 +16,8 @@ const User= require('./models/user');
 const campgroundRoutes =require ('./routes/campgrounds');
 const reviewRoutes= require('./routes/reviews');
 const userRoutes= require('./routes/users');
-
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
 
 
 // DATABASE
@@ -38,14 +39,18 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(mongoSanitize({
+    replaceWith: '_',
+}));
 
 
 const sessionConfig= {
+    name: 'session',
     secret: 'stuff',
     resave: false,
      cookie:{
         httpOnly:true,
+        // secure:true,
         expires: Date.now()+ 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7
      }
@@ -54,6 +59,56 @@ const sessionConfig= {
 
 app.use( session(sessionConfig));
 app.use(flash());
+app.use(
+    helmet()
+  );
+
+  const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [ 
+    "https://kit-free.fontawesome.com/" ,
+    "https://stackpath.bootstrapcdn.com/" ,
+    "https://api.mapbox.com/" ,
+    "https://api.tiles.mapbox.com/" ,
+    "https://fonts.googleapis.com/" ,
+    "https://use.fontawesome.com/" ,
+    "https://cdn.jsdelivr.net" ,
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dmvgmogaf/",
+                "https://res.cloudinary.com/dmjwhnvxf/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+                "https://res.cloudinary.com/dmjwhnvxf/"
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+  );
 
 
 app.use(passport.initialize());
@@ -66,7 +121,6 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res, next)=>{
-   
     res.locals.currentUser= req.user;
   res.locals.success=  req.flash('success');
   res.locals.error= req.flash('error');
